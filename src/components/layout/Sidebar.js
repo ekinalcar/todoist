a9967c3166f8064ae770191f342c3b2e11195e17
ekinalcar <ekinalcar@firmasoft.com>
2019-10-23 13:39:24 +0300

    context

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -1,39 +1,46 @@
-import React from 'react';
+import React, { useState } from "react";
 import {
- FaChevronDown,
- FaInbox,
- FaRegCalendarAlt,
- FaRegCalendar,
-} from 'react-icons/fa';
+  FaChevronDown,
+  FaInbox,
+  FaRegCalendarAlt,
+  FaRegCalendar,
+} from "react-icons/fa";
+import { useSelectedProjectValue } from "../../context";
 
 export const Sidebar = () => {
+  const { setSelectedProject } = useSelectedProjectValue;
+  const [active, setActive] = useState("inbox");
+  const [showProjects, setShowProjects] = useState(true);
+
   return (
     <div className="sidebar" data-test="sidebar">
-			<ul className="sidebar__generic">
-				<li>
-					<span>
-						<FaInbox />
-					</span>
-					<span>Inbox</span>
-				</li>
-				<li>
-					<span>
-						<FaRegCalendar />
-					</span>
-					<span>Today</span>
-				</li>
-				<li>
-					<span>
-						<FaRegCalendarAlt />
-					</span>
-					<span>Next 7 days</span>
-				</li>
-			</ul>
-			<div className="sidebar__middle">
-				<span><FaChevronDown /></span>
-				<h2>Projects</h2>
-			</div>
-			<ul className="sidebar__projects">Projects will be here</ul>
-		</div>
+      <ul className="sidebar__generic">
+        <li>
+          <span>
+            <FaInbox />
+          </span>
+          <span>Inbox</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendar />
+          </span>
+          <span>Today</span>
+        </li>
+        <li>
+          <span>
+            <FaRegCalendarAlt />
+          </span>
+          <span>Next 7 days</span>
+        </li>
+      </ul>
+      <div className="sidebar__middle">
+        <span>
+          <FaChevronDown />
+        </span>
+        <h2>Projects</h2>
+      </div>
+      <ul className="sidebar__projects">Projects will be here</ul>
+    </div>
   );
 };
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,46 +1,48 @@
 /* eslint-disable no-nested-ternary */
-import { useState, useEffect } from 'react';
-import moment from 'moment';
-import { firebase } from '../firebase';
-import { collatedTasksExist } from '../helpers';
+import { useState, useEffect } from "react";
+import moment from "moment";
+import { firebase } from "../firebase";
+import { collatedTasksExist } from "../helpers";
 
-export const useTasks = (selectedProject) => {
+export const useTasks = selectedProject => {
   const [tasks, setTasks] = useState([]);
   const [archivedTasks, setArchivedTasks] = useState([]);
 
   useEffect(() => {
     let unsubscribe = firebase
       .firestore()
-      .collection('tasks')
-      .where('userId', '==', '1');
+      .collection("tasks")
+      .where("userId", "==", "1");
 
-    unsubscribe = selectedProject && !collatedTasksExist(selectedProject)
-        ? (unsubscribe = unsubscribe.where('projectId', '==', selectedProject))
-        : selectedProject === 'TODAY'
+    unsubscribe =
+      selectedProject && !collatedTasksExist(selectedProject)
+        ? (unsubscribe = unsubscribe.where("projectId", "==", selectedProject))
+        : selectedProject === "TODAY"
         ? (unsubscribe = unsubscribe.where(
-            'date',
-            '==',
-            moment().format('DD/MM/YYYY'),
+            "date",
+            "==",
+            moment().format("DD/MM/YYYY"),
           ))
-        : selectedProject === 'INBOX' || selectedProject === 0
-        ? (unsubscribe = unsubscribe.where('date', '==', ''))
+        : selectedProject === "INBOX" || selectedProject === 0
+        ? (unsubscribe = unsubscribe.where("date", "==", ""))
         : unsubscribe;
 
-    unsubscribe = unsubscribe.onSnapshot((snapshot) => {
-      const newTasks = snapshot.docs.map((task) => ({
+    unsubscribe = unsubscribe.onSnapshot(snapshot => {
+      const newTasks = snapshot.docs.map(task => ({
         id: task.id,
         ...task.data(),
       }));
 
       setTasks(
-        selectedProject === 'NEXT_7'
+        selectedProject === "NEXT_7"
           ? newTasks.filter(
-              (task) => moment(task.date, 'DD-MM-YYYY').diff(moment(), 'days') <= 7
-                && task.archived !== true,
+              task =>
+                moment(task.date, "DD-MM-YYYY").diff(moment(), "days") <= 7 &&
+                task.archived !== true,
             )
-          : newTasks.filter((task) => task.archived !== true),
+          : newTasks.filter(task => task.archived !== true),
       );
-      setArchivedTasks(newTasks.filter((task) => task.archived !== false));
+      setArchivedTasks(newTasks.filter(task => task.archived !== false));
     });
 
     return () => unsubscribe();
@@ -55,12 +57,12 @@ export const useProjects = () => {
   useEffect(() => {
     firebase
       .firestore()
-      .collection('projects')
-      .where('userId', '==', '1')
-      .orderBy('projectId')
+      .collection("projects")
+      .where("userId", "==", "1")
+      .orderBy("projectId")
       .get()
-      .then((snapshot) => {
-        const allProjects = snapshot.docs.map((project) => ({
+      .then(snapshot => {
+        const allProjects = snapshot.docs.map(project => ({
           ...project.data(),
           docId: project.id,
         }));
